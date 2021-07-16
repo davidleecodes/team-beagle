f7322789eda5f2ecb666a031a0eebeb6eb199271
concoctionfactory <david.g.lee.nyc@gmail.com>
2021-07-16 10:46:47 -0400

    testing server path

---
+++ client/src/helpers/APICalls/server.ts
@@ -0,0 +1,7 @@
+// let serverPath;
+
+// if (process.env.NODE_ENV === 'development') serverPath = 'http://localhost:3001';
+// else serverPath = 'https://hatchway-team-beagle.herokuapp.com/';
+const serverPath = 'https://hatchway-team-beagle.herokuapp.com/';
+
+export default serverPath;
