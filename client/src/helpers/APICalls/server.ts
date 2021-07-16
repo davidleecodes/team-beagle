f1325cecc2f968be3d77748f197b3b812f8763c0
concoctionfactory <david.g.lee.nyc@gmail.com>
2021-07-16 12:47:58 -0400

    Update server.ts

--- client/src/helpers/APICalls/server.ts
+++ client/src/helpers/APICalls/server.ts
@@ -1,6 +1,6 @@
 let serverPath: string;
 if (process.env.NODE_ENV === 'development') serverPath = 'http://localhost:3001';
-else serverPath = 'https://hatchway-team-beagle.herokuapp.com/';
+else serverPath = 'https://hatchway-team-beagle.herokuapp.com';
 // const serverPath = 'https://hatchway-team-beagle.herokuapp.com';
 
 export default serverPath;
