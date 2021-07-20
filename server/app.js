14a2a22d37d78f184492fc3035c78450aa8aa89f
concoctionfactory <david.g.lee.nyc@gmail.com>
2021-07-19 22:00:06 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -34,48 +34,6 @@ app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PATCH, DELETE,OPTIONS "
-//   );
-//   res.setHeader("Access-Control-Allow-Credentials", "true");
-//   next();
-// });
-
-// app.use((req, res, next) => {
-//   const corsWhitelist = [
-//     "http://localhost:3000",
-//     "https://60f1b846284ec600074f42b1--tender-tereshkova-6d8aaa.netlify.app",
-//   ];
-//   console.log(req.headers.origin);
-//   console.log(corsWhitelist.indexOf(req.headers.origin) !== -1);
-
-//   if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
-//     res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
-//     res.setHeader(
-//       "Access-Control-Allow-Headers",
-//       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//     );
-//     res.setHeader(
-//       "Access-Control-Allow-Methods",
-//       "GET, POST, PATCH, DELETE,OPTIONS"
-//     );
-//     res.setHeader("Access-Control-Allow-Credentials", "true");
-//   }
-//   next();
-// });
-// const corsConfig = {
-//   credentials: true,
-//   origin: true,
-// };
-// app.use(cors(corsConfig));
-
 const corsConfig = {
   credentials: true,
   origin: true,
@@ -96,17 +54,9 @@ app.use("/message", messageRouter);
 app.use("/payment", paymentRouter);
 app.use("/review", reviewRouter);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname), "client", "build", "index.html")
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running");
-  });
-}
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
 app.use(notFound);
 app.use(errorHandler);
